fe5aaf399ef6fa6e400fb55966b1f5fe06f175a3
Sharara <shararasaminkhan@gmail.com>
2025-03-05 13:26:05 +0600

    Initial commit

---
+++ 2/2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter name");
+        Console.WriteLine("Hello " + Console.ReadLine()); //string concatenation
+
+        Console.WriteLine("name please");
+        Console.WriteLine("hi {0}", Console.ReadLine()); //placeholder
+    }
+}
